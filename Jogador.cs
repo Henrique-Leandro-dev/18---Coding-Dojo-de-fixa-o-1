f4d11382064167aec8148343a562238bdd40c793
Henrique <miniqui10@gmail.com>
2020-07-14 00:00:12 -0300

    Atividade dojo1 finalizada

---
+++ Jogador.cs
@@ -0,0 +1,58 @@
+using System;
+namespace Aula18_dojo
+{  
+
+    public class Jogador
+    {
+        
+   public string Nome { get;set; }
+        public int Idade { get;set; }
+        public string Nacionalidade { get;set; }
+        public string Posicao { get;set; }
+        public DateTime DataDeNascimento { get;set; }
+        public float Peso { get;set; }
+        public float Altura { get;set; }
+        
+        public int CalcularIdade(){
+
+            int AnoAtual = Int32.Parse(DateTime.Now.ToString().Split ("/") [2].Split (" ") [0]);
+            int DataNascimento = Int32.Parse(DataDeNascimento.ToString().Split("/")[2].Split(" ") [0]  );
+
+            Idade = AnoAtual - DataNascimento;
+
+            return Idade;
+        
+        }
+
+        public string VerificarAposentadoria(string Posicao, string Nome){
+            
+            string Mensagem = "";
+
+            if (Idade >= 35 && Posicao == "ATA")
+            {
+                Mensagem = $"{Nome} deve se aposentar";
+
+            }else if (Idade >= 38 && Posicao == "MC"){
+
+                Mensagem = $"{Nome} deve se aposentar";
+
+            }else if (Idade >= 40 && Posicao == "DEF"){
+
+                Mensagem = $"{Nome} deve se aposentar"; 
+
+            }else if (Idade >= 0 && Idade <=16){
+
+                Mensagem = $"{Nome} ainda não é um jogador profissional";
+
+            }else{
+
+                Mensagem = $"{Nome} não atingiu a idade suficiente para se aposentar!";
+            }
+            
+            return Mensagem; 
+
+        }
+
+
+    }
+}  
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Aula18_dojo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+     Jogador fred = new Jogador();
+
+            System.Console.WriteLine("Qual o nome do jogador?");
+            fred.Nome = Console.ReadLine();
+            Console.Clear();
+            
+            System.Console.WriteLine("Qual a nacionalidade?");
+            fred.Nacionalidade = Console.ReadLine();
+            Console.Clear();
+
+            System.Console.WriteLine($"Qual o peso do {fred.Nome} atual?");
+            fred.Peso = float.Parse(Console.ReadLine());
+            Console.Clear();
+
+            System.Console.WriteLine($"Qual a altura do {fred.Nome}?");
+            fred.Altura = float.Parse(Console.ReadLine());
+            Console.Clear();
+
+            System.Console.WriteLine($"Qual a data de nascimento do {fred.Nome} (dia/mês/ano)?");
+            fred.DataDeNascimento = DateTime.Parse(Console.ReadLine());
+            Console.Clear();
+            
+            System.Console.WriteLine($"Em qual posição o {fred.Nome} joga (escolha somente uma)?\nATA = atacante | MC = meio-campista | DEF = defensor");
+            fred.Posicao = Console.ReadLine();
+            Console.Clear();
+
+            System.Console.WriteLine(fred.CalcularIdade());
+            System.Console.WriteLine(fred.VerificarAposentadoria(fred.Posicao, fred.Nome));
+        }
+    }
+}
